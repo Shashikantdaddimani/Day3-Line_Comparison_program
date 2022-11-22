c753c72c9d8598fd83c96e5daa82a35bb9411f75
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2022-11-22 13:24:38 +0530

    Added:Welcome message of Line comparison program

---
+++ src/com/java/linecomparison/LineComparison.java
@@ -0,0 +1,10 @@
+package com.java.linecomparison;
+
+public class LineComparison {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the line comparison computation program ");
+
+	}
+
+}
